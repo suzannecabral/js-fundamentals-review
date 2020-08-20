71a95c54ee2a5ea07f06768ca50eaecf65e5897c
Buddy Agyin <agyin3@gmail.com>
2020-08-20 13:30:19 -0600

    add js tasks

---
+++ classes.js
@@ -0,0 +1,31 @@
+/* 
+We have a school to build here! In this project you will get used to thinking about classes in JavaScript and building them from a brand new data set.
+*/
+
+/* 
+Person
+ - First we need a Person class. This will be our base-class
+ - Person receives name age location all as props
+ - Person receives speak as a method.
+    - This method returns a phrase Hello my name is Fred, I am from Bedrock where name and location are the object's own props
+
+Professor
+ - Now that we have a Person as our base class, we'll build our Professor class.
+ - Professor uses the same attributes that have been set up by Person
+ - Professor has the following unique props:
+     - subject what the Professor is good at i.e. 'Kanye West and his impact on modern hip-hop'
+     - catchPhrase i.e. Don't forget the homies
+ - Professor has the following methods:
+     - greet returns the Professor's own catch-phrase
+*/
+
+
+
+/* Uncomment below to test your code */
+
+// const snape = new Professor("Severus Snape", 32, "Hogwarts", "Potions", "10 points from Gryffindor")
+
+// console.log(snape.speak()) // Hello my name is Severus Snape, I am from Hogwarts
+// console.log(snape.greet()) // "10 points from Gryffindor"
+// console.log(snape.age) // 32
+
